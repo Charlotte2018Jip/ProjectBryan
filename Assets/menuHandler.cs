08cb1ef4f3c4f45b770442afafc132c76106dce3
Charrie <charlotte.oers@kpnmail.nl>
2020-09-15 10:48:54 +0200

    panel

---
+++ Assets/menuHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuHandler : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void openPanel()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);
+
+        }
+    }
+}

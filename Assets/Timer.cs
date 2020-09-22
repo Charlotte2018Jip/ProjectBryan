5699d2e673a5b6ef5e137155b253fe0e5f7fca69
LKohnle <63394971+LKohnle@users.noreply.github.com>
2020-09-22 09:58:51 +0200

    Timer
    Tussen Comments

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    public float MaxTime = 60f;
-    [SerializeField]
-    private float CountDown = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        CountDown = MaxTime;
-    }
+    //public float MaxTime = 60f;
+    //[SerializeField]
+    //private float CountDown = 0;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    CountDown = MaxTime;
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        CountDown -= Time.DeltaTime;
-        if (CountDown <= 0)
-        {
-            Coin.CoinCount = 0;
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    CountDown -= Time.DeltaTime;
+    //    if (CountDown <= 0)
+    //    {
+    //        Coin.CoinCount = 0;
             
-        }
-    }
+    //    }
+    //}
 }

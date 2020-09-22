1e4ec59b76378b0bca949fa05bf5fd34e5531b11
Charrie <charlotte.oers@kpnmail.nl>
2020-09-22 11:36:28 +0200

    script voor levels unclocken

---
+++ Assets/LevelSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Sla op of het level beschikbaar is of niet
+  public void SaveLevel(int id )
+    {
+        PlayerPrefs.SetInt("level", id);
+    }
+}
+public class LevelSaver : MonoBehaviour
+{
+    //This field should be set by the inspector
+    [SerializeField] int levelRequirement;
+    public void Start()
+    {
+        
+    }
+}
+
+
+

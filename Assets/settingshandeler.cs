9327c0c54a01e720454adf76162126e77568e260
SterreVriens <Sterre.vriens03@gmail.com>
2020-09-22 11:01:53 +0200

    Setting done

--- Assets/settingshandeler.cs
+++ Assets/settingshandeler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class settingshandeler : MonoBehaviour
 {
@@ -12,5 +13,24 @@ public class settingshandeler : MonoBehaviour
     {
         AudioMixer.SetFloat("volume", volume);
     }
+    public void SetFullscreen(bool toggle)
+    {
+        // Toggle fullscreen
+        Screen.fullScreen = !Screen.fullScreen;
+
+        
+    }
+
+    public void gotohome()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void gotolevel()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
 
 }
+
+

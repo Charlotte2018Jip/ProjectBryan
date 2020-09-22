afcaef6d2a6ce5cd7b983e418a9ec805f2f4935e
Charrie <charlotte.oers@kpnmail.nl>
2020-09-22 11:49:43 +0200

    levelselection

--- Assets/LevelSelection.cs
+++ Assets/LevelSelection.cs
@@ -1,26 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class enableLevel : MonoBehaviour
+public class LevelSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button[] lvlButtons;
 
-    //Sla op of het level beschikbaar is of niet
-  public void SaveLevel(int id )
+    private void Start()
     {
-        PlayerPrefs.SetInt("level", id);
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2);
+        for (int i = 0; i < lvlButtons.Length; i++)
+        {
+            if (i + 2 > levelAt)
+            {
+                lvlButtons[i].interactable = false;
+            }
+        }
     }
+
 }
-public class LevelSaver : MonoBehaviour
-{
-    //This field should be set by the inspector
-    [SerializeField] int levelRequirement;
-    public void Start()
-    {
-        
-    }
-}
+
 
 
 

08453a35d90ad5c44f4537011512956dbd883ccc
Charrie <charlotte.oers@kpnmail.nl>
2020-10-13 09:59:49 +0200

    commit

---
+++ Assets/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreCounter : MonoBehaviour
+{
+    public static int score;
+    public Text scoreText;
+    
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        score = 0;
+    }
+    void Update()
+    {
+
+        scoreText.text = "Score: "  + score;
+    }
+
+   
+}
